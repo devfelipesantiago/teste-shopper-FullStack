2d5f04ce30854b9bc70a1162b5994dd8db39f075
felipe santiago <devfelipesantiago>
2023-09-07 13:50:07 -0300

    Feat: connection DB

--- backend/src/db/connection.js
+++ backend/src/db/connection.js
@@ -5,7 +5,7 @@ const connection = mysql.createPool({
   port: process.env.MYSQL_PORT || 33060,
   user: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || 'root',
-  database: process.env.DB_NAME || 'database',
+  database: process.env.DB_NAME || 'shopperTest',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
